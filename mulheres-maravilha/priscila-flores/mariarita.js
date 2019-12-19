9155a3ef455c4469c5fee9e75e38deb4f81f8f3a
Priscila Uchoa <priscilauchoaa@gmail.com>
2019-12-19 23:56:51 +0100

    Configuração menu responsivo

--- mulheres-maravilha/priscila-flores/mariarita.js
+++ mulheres-maravilha/priscila-flores/mariarita.js
@@ -1,3 +1,32 @@
+
+(function() {
+    var menuResponsivo = document.getElementById("menuResponsivo");
+    var btnResponsivo = document.getElementById("btnResponsivo");
+  
+    btnResponsivo.addEventListener("click", function(e) {
+      e.preventDefault();
+      document.body.scrollTop += 0;
+      if (menuResponsivo.className === "navegacao__itens") {
+        menuResponsivo.className += " ativo";
+      } else {
+        menuResponsivo.className = "navegacao__itens";
+      }
+    });
+  })();
+  
+  window.addEventListener("scroll", function() {
+    if (pageYOffset >= 250) {
+      document.querySelector(".navegacao").style.backgroundColor = "#674582";
+      document.querySelector(".navegacao__itens").style.backgroundColor =
+        "#674582";
+    } else {
+      document.querySelector(".navegacao").style.backgroundColor = "transparent";
+      document.querySelector(".navegacao__itens").style.backgroundColor =
+        "transparent";
+    }
+  });
+
+
 const titulo = document.querySelector('#principal__titulo');
 typeWriter(titulo);
 
@@ -18,3 +47,4 @@ $(document).ready(function () {
         $('.autora__apresentacao').toggle('slow'); 
     });
 })
+
